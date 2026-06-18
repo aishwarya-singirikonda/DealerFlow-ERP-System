0049a8f166ac56530da711ba6570fa9ecb12ee78
Aishwarya Singirikonda <sr8005aishwarya@gmail.com>
2026-06-18 11:29:44 +0530

    Add api path

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://dealerflow-erp-system.onrender.com",
+  baseURL: "https://dealerflow-erp-system.onrender.com/api",
 });
 
 export default API;
